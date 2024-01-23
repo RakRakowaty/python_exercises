5b1b86a6010a05efb5f61b9625434a8804116d47
RakRakowaty <mati.kruk1@gmail.com>
2024-01-23 12:05:33 +0100

    task6

---
+++ tasks/task6.py
@@ -0,0 +1,15 @@
+def sum1(x):
+    O=0
+    for i in x:
+        O += i
+    return O
+    
+print(sum1([2,8,3,5]))
+
+def multyply(x):
+    A=1
+    for i in x:
+        A *= i 
+    return A
+
+print(multyply([2,8,3,5]))
